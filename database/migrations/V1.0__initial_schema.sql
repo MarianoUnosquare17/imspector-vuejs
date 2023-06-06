08f3341e16150ca1597af3fe2be63cf46e044f93
Mariano Estrada <mariano.estrada@vesta.io>
2023-06-06 13:55:37 -0600

    added migrations and seed folders

--- database/migrations/V1.0__initial_schema.sql
+++ database/migrations/V1.0__initial_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.accounts
 (
-    id serial constraint id_pk primary key,
+    account_id serial constraint id_account_pk primary key,
     username varchar not null,
     email varchar not null,
     password varchar not null
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS public.accounts
 
 CREATE TABLE IF NOT EXISTS public.players
 (
-    id serial constraint id_player_pk primary key,
+    player_id serial constraint id_players_pk primary key,
     valorant_account varchar not null,
     valorant_tag varchar not null,
     level int not null,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS public.players
 
 CREATE TABLE IF NOT EXISTS public.tactics
 (
-    id serial constraint id_tactics_pk primary key,
+    tactic_id serial constraint id_tactic_pk primary key,
     created_by varchar not null,
     map_id varchar not null,
     agent_id varchar not null,
@@ -27,26 +27,26 @@ CREATE TABLE IF NOT EXISTS public.tactics
 
 CREATE TABLE IF NOT EXISTS public.maps
 (
-    id serial constraint id_tactics_pk primary key,
+    map_id serial constraint id_map_pk primary key,
     name varchar not null
 );
 
 CREATE TABLE IF NOT EXISTS public.agents
 (
-    id serial constraint id_tactics_pk primary key,
+    agent_id serial constraint id_agent_pk primary key,
     name varchar not null
 );
 
 CREATE TABLE IF NOT EXISTS public.modes
 (
-    id serial constraint id_tactics_pk primary key,
+    mode_id serial constraint id_mode_pk primary key,
     name varchar not null
 );
 
 CREATE TABLE IF NOT EXISTS public.comments
 (
-    id serial constraint id_game_comments_pk primary key,
-    comment varchar not null
+    comment_id serial constraint id_game_comment_pk primary key,
+    comment varchar not null,
     player_match_id int not null,
     date_created date not null,
     created_by varchar not null
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS public.comments
 
 CREATE TABLE IF NOT EXISTS public.player_matches
 (
-    id serial constraint id_acc_games_pk primary key,
+    match_id serial constraint id_acc_match_pk primary key,
     score int not null,
     date_played date not null,
     kills int not null,
